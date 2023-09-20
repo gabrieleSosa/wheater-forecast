276540a9d6fd83a9d11ce0aaab9050104c36b1d8
Gabriele Di Cosmo <gabrieledicosmo05@gmail.com>
2023-09-20 12:49:47 +0200

    inserimento autorizzazioni

--- static/js/leafletScript.js
+++ static/js/leafletScript.js
@@ -1,31 +1,45 @@
-let latinput = document.querySelector("#lat");
-let lnginput = document.querySelector("#lng");
+function createMap(lat, lng) {
+	let latinput = document.querySelector("#lat");
+	let lnginput = document.querySelector("#lng");
 
-var map = L.map("map").setView([51.505, -0.09], 13);
+	var map = L.map("map").setView([lat, lng], 13);
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-	maxZoom: 19,
-	attribution:
-		'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+	L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+		maxZoom: 19,
+		attribution:
+			'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	}).addTo(map);
 
-var popup = L.popup();
-var marker = L.marker();
+	var popup = L.popup();
+	var marker = L.marker();
 
-function onMapClick(e) {
-	map.removeLayer(marker);
-	marker = L.marker(e.latlng).addTo(map);
+	function onMapClick(e) {
+		map.removeLayer(marker);
+		marker = L.marker(e.latlng).addTo(map);
 
-	popup
-		.setLatLng(e.latlng)
-		.setContent("You clicked the map at " + e.latlng.toString())
-		.openOn(map);
+		popup
+			.setLatLng(e.latlng)
+			.setContent("You clicked the map at " + e.latlng.toString())
+			.openOn(map);
 
-	let lat = e.latlng.lat.toString();
-	let lng = e.latlng.lng.toString();
+		let lat = e.latlng.lat.toString();
+		let lng = e.latlng.lng.toString();
 
-	latinput.value = lat;
-	lnginput.value = lng;
+		latinput.value = lat;
+		lnginput.value = lng;
+	}
+
+	map.on("click", onMapClick);
 }
 
-map.on("click", onMapClick);
+navigator.geolocation.getCurrentPosition(
+	function (event) {
+		console.log("l'utente ha accettato")
+		createMap(event.coords.latitude, event.coords.longitude)
+	},
+
+	function (event) {
+		console.log("l'utente non ha accettato")
+		createMap(51, -0.09)
+	}
+)
--- static/js/script.js
+++ static/js/script.js
@@ -4,13 +4,9 @@ resetButton.addEventListener("click", function () {
 	location.reload();
 });
 
-let dataContainer = document.querySelectorAll('.data-container')
+let dataContainer = document.querySelectorAll(".data-container");
 
 document.querySelector("form").addEventListener("submit", function (event) {
-
-
-	
-
 	event.preventDefault();
 	let latitudine = document.querySelector("#lat").value;
 	let longitudine = document.querySelector("#lng").value;
@@ -28,19 +24,18 @@ document.querySelector("form").addEventListener("submit", function (event) {
 		.then(function (data) {
 			console.log(data.hourly.time);
 			console.log(data.hourly.temperature_2m);
-			
 
 			for (let i = 0; i < 9; i++) {
 				let timeElement = document.querySelectorAll("span.time");
-				timeElement[i].innerHTML = `${data.hourly.time[i]}`
+				timeElement[i].innerHTML = `${data.hourly.time[i]}`;
 
-				let temperatureElement = document.querySelectorAll("span.temp")
+				let temperatureElement = document.querySelectorAll("span.temp");
 				temperatureElement[i].innerHTML = `${data.hourly.temperature_2m[i]}`;
 			}
 
 			for (let i = 0; i < dataContainer.length; i++) {
-
-				dataContainer[i].classList.add('border-2')
+				dataContainer[i].classList.add("border-2");
 			}
 		});
 });
+
