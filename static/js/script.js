dd0c04f0e6cf43c506c4a74ccb1e387f35dc9353
Gabriele Di Cosmo <gabrieledicosmo05@gmail.com>
2023-09-21 18:57:28 +0200

    inizio grafico

--- static/js/script.js
+++ static/js/script.js
@@ -11,21 +11,66 @@ document.querySelector("form").addEventListener("submit", function (event) {
 	let latitudine = document.querySelector("#lat").value;
 	let longitudine = document.querySelector("#lng").value;
 
-	console.log(latitudine, longitudine);
-
-	let url = `https://api.open-meteo.com/v1/forecast?latitude=${latitudine}&longitude=${longitudine}&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,windspeed_10m`;
-
-	console.log(url);
+	let url = `https://api.open-meteo.com/v1/forecast?latitude=${latitudine}&longitude=${longitudine}&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`;
 
 	fetch(url)
 		.then(function (resp) {
 			return resp.json();
 		})
 		.then(function (data) {
-			console.log(data.hourly.time);
-			console.log(data.hourly.temperature_2m);
+			let timeArr = [];
+			let tempeArr = [];
+			let humidityArr = [];
+			let windspeedArr = [];
 
-			//dati dell'API
+			for (let i = 0; i < 30; i++) {
+				timeArr[i] = data.hourly.time[i];
+				tempeArr[i] = data.hourly.temperature_2m[i];
+				humidityArr[i] = data.hourly.relativehumidity_2m[i];
+				windspeedArr[i] = data.hourly.windspeed_10m[i];
+			}
+
+			createChart();
 		});
 });
 
+function createChart() {
+	let canvas = document.querySelector("canvas");
+	canvas.classList.add("canvasSytle");
+	let config = {
+		type: "line",
+		data: {
+			labels: ["January", "February", "March"],
+			datasets: [
+				{
+					label: "Temperatura media",
+					data: [10, 20, 11.2],
+					fill: false,
+					borderColor: "rgb(75, 192, 192)",
+					tension: 0.1,
+					yAxisID: "y",
+				},
+				{
+					label: "Umidità media",
+					data: [70, 72, 66],
+					fill: false,
+					borderColor: "rgb(255, 0, 0)",
+					tension: 0.1,
+					yAxisID: "y1",
+				},
+			],
+		},
+		scales: {
+			y: {
+				type: "linear",
+				position: "left",
+			},
+			y1: {
+				type: "linear",
+				position: "left",
+			},
+		},
+	};
+
+	const myChart = new Chart(canvas, config);
+}
